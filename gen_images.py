8223def12b1da41211fbd6d0438adc878ec61a29
TsungWing <38560218+W0n9@users.noreply.github.com>
2022-08-31 11:48:35 +0800

    Generate image grid

--- gen_images.py
+++ gen_images.py
@@ -133,7 +133,7 @@ def generate_images(
             G.synthesis.input.transform.copy_(torch.from_numpy(m))
 
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        img: torch.Tensor = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.jpg')
 
 
